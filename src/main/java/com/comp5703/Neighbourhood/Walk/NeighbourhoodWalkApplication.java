3007126d5541a7f6de42159632ddbfcee84f7330
Calvin Li <jili8520@uni.sydney.edu.au>
2024-08-18 11:36:00 +1000

    Springboot boilerplate

---
+++ src/main/java/com/comp5703/Neighbourhood/Walk/NeighbourhoodWalkApplication.java
@@ -0,0 +1,13 @@
+package com.comp5703.Neighbourhood.Walk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighbourhoodWalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighbourhoodWalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comp5703/Neighbourhood/Walk/NeighbourhoodWalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp5703.Neighbourhood.Walk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighbourhoodWalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
