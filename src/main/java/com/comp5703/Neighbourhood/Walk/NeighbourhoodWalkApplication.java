6a06fb267dcd1cb894fd2a7e2d569dc48edd75aa
Dream0041011 <bolu0423@uni.sydney.edu.au>
2024-08-21 17:38:46 +1000

    Update-Comment module-Bolun
    change a little things

--- src/main/java/com/comp5703/Neighbourhood/Walk/NeighbourhoodWalkApplication.java
+++ src/main/java/com/comp5703/Neighbourhood/Walk/NeighbourhoodWalkApplication.java
@@ -1,6 +1,5 @@
 package com.comp5703.Neighbourhood.Walk;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
