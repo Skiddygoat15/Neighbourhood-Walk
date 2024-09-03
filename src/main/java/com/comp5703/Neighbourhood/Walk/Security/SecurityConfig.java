205af85abcc2cc1b894e1af514365af5b8a367f8
Calvin Li <jili8520@uni.sydney.edu.au>
2024-09-03 21:42:02 +1000

    Security Filter Chain in progress

---
+++ src/main/java/com/comp5703/Neighbourhood/Walk/Security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.comp5703.Neighbourhood.Walk.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("admin-pass")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+}
