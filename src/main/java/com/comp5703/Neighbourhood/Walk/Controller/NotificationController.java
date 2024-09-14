40b64404c00ef3d4ac261b9e25b51fd3176c6a76
Dream0041011 <bolu0423@uni.sydney.edu.au>
2024-09-14 20:40:27 +1000

    Update NotificationController.java

--- src/main/java/com/comp5703/Neighbourhood/Walk/Controller/NotificationController.java
+++ src/main/java/com/comp5703/Neighbourhood/Walk/Controller/NotificationController.java
@@ -4,7 +4,6 @@ import com.comp5703.Neighbourhood.Walk.Entities.Notification;
 import com.comp5703.Neighbourhood.Walk.Service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,7 +18,7 @@ public class NotificationController {
     public ResponseEntity<Notification> addNotification(@RequestBody Notification notification){
         return new ResponseEntity<>(notificationService.addNotification(notification), HttpStatus.OK);
     }
-    
+
     @GetMapping("/findNotificationByWalkerRequestId/{walkerRequestId}")
     public ResponseEntity<Notification> findNotificationByWalkerRequestId(@PathVariable("walkerRequestId") long walkerRequestId) {
         return new ResponseEntity<>(notificationService.findNotificationByWalkerRequestId(walkerRequestId),HttpStatus.OK);
