b30004da2b8e9367f87a81f6f938ad1036ade5a0
Dream0041011 <bolu0423@uni.sydney.edu.au>
2024-09-15 22:10:45 +1000

    update

--- frontend/src/components/StatusCard.js
+++ frontend/src/components/StatusCard.js
@@ -2,6 +2,31 @@
 import React, {useState} from 'react';
 
 export default function StatusCard({ parentId, walkerId, status, time }) {
+    const myInit = {
+        method: 'GET',
+        headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')},
+        mode: 'cors',
+        cache: 'default'
+    };
+
+    const [role, setRole] = useState('parent');
+
+    const requestURL = new Request(`http://127.0.0.1:8080/WalkerRequest/getWalkerRequestByWalkerId/${walkerId}`, myInit);
+    fetch(requestURL)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            console.info(response.json())
+            return response.json();
+        })
+        .then(data => {
+            if (data.status === "Accepted" || data.status === "Rejected" || data.status === "Pending...") {
+                setRequestStatus(data.status);
+            }})
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
 
     const [showDot, setShowDot] = useState(true);  // 初始状态为显示小红点
 
