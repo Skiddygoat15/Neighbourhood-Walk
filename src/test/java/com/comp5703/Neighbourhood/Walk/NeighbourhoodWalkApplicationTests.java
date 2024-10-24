c983aae766bda9f9e49db578523948925cdb619e
11shiyu <90696374+11shiyu@users.noreply.github.com>
2024-10-24 19:29:46 +1100

    update the front/back end environment setting to fit deploy environment

--- src/test/java/com/comp5703/Neighbourhood/Walk/NeighbourhoodWalkApplicationTests.java
+++ src/test/java/com/comp5703/Neighbourhood/Walk/NeighbourhoodWalkApplicationTests.java
@@ -2,8 +2,10 @@ package com.comp5703.Neighbourhood.Walk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 class NeighbourhoodWalkApplicationTests {
 
 	@Test
